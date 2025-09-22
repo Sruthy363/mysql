7cd76041019b84ac618b987d37817d7d294b3c0c
Sruthy363 <sruthy363ms@gmail.com>
2025-09-22 10:44:33 +0530

    homework1

--- homework/homework1.sql
+++ homework/homework1.sql
@@ -1,14 +1,29 @@
-CREATE TABLE student_info (
-    id INT PRIMARY KEY ,
-    name TEXT(50) NOT NULL,
-    age INT ( 90),
-    department TEXT(20),
-    grade INT(52)
+CREATE TABLE bookss (
+    id INT PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    price DECIMAL(10,2),
+    genre VARCHAR(100)
 );
 
 
-INSERT INTO student_info (id, name, age, department, grade)
- VALUES ('1','Alfred','23','computer science','80'), 
- ('2','Kiran','19','physics','90'), 
- ('3','Chong','34','biology','57'), 
- ('4','Suresh','17','maths','69');
+INSERT INTO bookss (id, title, author, price, genre)
+VALUES 
+(1, 'The Great Gatsby', 'F. Scott Fitzgerald', 500, 'Fiction'),
+(2, 'A Brief History of Time', 'Stephen Hawking', 700, 'Science'),
+(3, 'The Da Vinci Code', 'Dan Brown', 450, 'Mystery'),
+(4, 'Sapiens', 'Yuval Noah Harari', 650, 'History'),
+(5, 'To Kill a Mockingbird', 'Harper Lee', 350, 'Fiction');
+
+SELECT * FROM bookss WHERE price > 400 ;
+
+
+SELECT * FROM bookss
+WHERE genre IN ('History', 'Science', 'Fiction');
+
+SELECT * 
+FROM books
+WHERE title = 'The Great Gatsby';
+
+SELECT * 
+FROM books WHERE author <> 'Dan Brown';
